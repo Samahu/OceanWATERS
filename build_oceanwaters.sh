93284273a8818debbc891a8ec2964ff6090776e9
Ussama Naal <ussama.naal@nasa.gov>
2020-08-06 19:40:09 -0700

    define PLEXIL_HOME before referencing it again

--- build_oceanwaters.sh
+++ build_oceanwaters.sh
@@ -2,7 +2,10 @@
 
 # this script assumes that we have already checked out all required repositories into src folder
 source /opt/ros/melodic/setup.bash
+
+# source plexil again
+export PLEXIL_HOME=$(pwd)/plexil
 source $PLEXIL_HOME/scripts/plexil-setup.sh
 
 catkin init
-catkin build
+catkin build --cmake-args -Wno-dev

efcff0585647d9bf780818e59a3dc52566948ab7
Ussama Naal <ussama.naal@nasa.gov>
2021-01-26 15:50:00 -0800

    Pull latest changes + Remove PLEXIL commit requirement

--- build_plexil.sh
+++ build_plexil.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 git clone https://git.code.sf.net/p/plexil/git /plexil
-# update to latest commit known to compile successfully
-pushd /plexil
-git checkout 51813f1f6846d37b401bc088f0f5aeb5debb9841
-popd
 
 export PLEXIL_HOME=/plexil
 source $PLEXIL_HOME/scripts/plexil-setup.sh

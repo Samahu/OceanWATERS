2ba06bc4e55df1f677efecbc1e97d56b131277a4
Ussama Naal <ussama.naal@nasa.gov>
2021-05-10 14:47:21 -0700

    Checkout GSAP using the v1.0-OW tag

--- build_gsap.bash
+++ build_gsap.bash
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 git config --global http.postBuffer 1048576000
-git clone --depth 1 --branch v2.0.0 https://github.com/nasa/GSAP.git /gsap
+git clone --depth 1 --branch v1.0-OW https://github.com/nasa/GSAP.git /gsap
 
 
 export GSAP_HOME=/gsap
